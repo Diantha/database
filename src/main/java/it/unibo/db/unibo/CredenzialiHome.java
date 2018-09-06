a15f4cdc15ea0ba049ecc663a24c72a4961d5176
Liz <>
2018-09-06 18:33:25 +0200

    fix pom

---
+++ src/main/java/it/unibo/db/unibo/CredenzialiHome.java
@@ -0,0 +1,74 @@
+package it.unibo.db.unibo;
+// Generated 6-set-2018 18.31.48 by Hibernate Tools 4.3.2-SNAPSHOT
+
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Home object for domain model class Credenziali.
+ * @see it.unibo.db.unibo.Credenziali
+ * @author Hibernate Tools
+ */
+@Stateless
+(name="unibo_CredenzialiHome") 
+public class CredenzialiHome {
+
+    private static final Log log = LogFactory.getLog(CredenzialiHome.class);
+
+    @PersistenceContext(unitName="unibo") private EntityManager entityManager;
+    
+    public void persist(Credenziali transientInstance) {
+        log.debug("persisting Credenziali instance");
+        try {
+            entityManager.persist(transientInstance);
+            log.debug("persist successful");
+        }
+        catch (RuntimeException re) {
+            log.error("persist failed", re);
+            throw re;
+        }
+    }
+    
+    public void remove(Credenziali persistentInstance) {
+        log.debug("removing Credenziali instance");
+        try {
+            entityManager.remove(persistentInstance);
+            log.debug("remove successful");
+        }
+        catch (RuntimeException re) {
+            log.error("remove failed", re);
+            throw re;
+        }
+    }
+    
+    public Credenziali merge(Credenziali detachedInstance) {
+        log.debug("merging Credenziali instance");
+        try {
+            Credenziali result = entityManager.merge(detachedInstance);
+            log.debug("merge successful");
+            return result;
+        }
+        catch (RuntimeException re) {
+            log.error("merge failed", re);
+            throw re;
+        }
+    }
+    
+    public Credenziali findById( Integer id) {
+        log.debug("getting Credenziali instance with id: " + id);
+        try {
+            Credenziali instance = entityManager.find(Credenziali.class, id);
+            log.debug("get successful");
+            return instance;
+        }
+        catch (RuntimeException re) {
+            log.error("get failed", re);
+            throw re;
+        }
+    }
+}
+
---
+++ src/main/java/it/unibo/db/unibo/UtenteHome.java
@@ -0,0 +1,74 @@
+package it.unibo.db.unibo;
+// Generated 6-set-2018 18.31.48 by Hibernate Tools 4.3.2-SNAPSHOT
+
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Home object for domain model class Utente.
+ * @see it.unibo.db.unibo.Utente
+ * @author Hibernate Tools
+ */
+@Stateless
+(name="unibo_UtenteHome") 
+public class UtenteHome {
+
+    private static final Log log = LogFactory.getLog(UtenteHome.class);
+
+    @PersistenceContext(unitName="unibo") private EntityManager entityManager;
+    
+    public void persist(Utente transientInstance) {
+        log.debug("persisting Utente instance");
+        try {
+            entityManager.persist(transientInstance);
+            log.debug("persist successful");
+        }
+        catch (RuntimeException re) {
+            log.error("persist failed", re);
+            throw re;
+        }
+    }
+    
+    public void remove(Utente persistentInstance) {
+        log.debug("removing Utente instance");
+        try {
+            entityManager.remove(persistentInstance);
+            log.debug("remove successful");
+        }
+        catch (RuntimeException re) {
+            log.error("remove failed", re);
+            throw re;
+        }
+    }
+    
+    public Utente merge(Utente detachedInstance) {
+        log.debug("merging Utente instance");
+        try {
+            Utente result = entityManager.merge(detachedInstance);
+            log.debug("merge successful");
+            return result;
+        }
+        catch (RuntimeException re) {
+            log.error("merge failed", re);
+            throw re;
+        }
+    }
+    
+    public Utente findById( Integer id) {
+        log.debug("getting Utente instance with id: " + id);
+        try {
+            Utente instance = entityManager.find(Utente.class, id);
+            log.debug("get successful");
+            return instance;
+        }
+        catch (RuntimeException re) {
+            log.error("get failed", re);
+            throw re;
+        }
+    }
+}
+
